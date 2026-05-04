75ef832321ea2116c67591571ccbae52c06b4ee0
Omar-A-Elnady <omarelnady144@gmail.com>
2026-05-04 16:57:35 +0300

    Update setup.py
    Fix setup.py configuration

--- ros_ws/src/line_follower/setup.py
+++ ros_ws/src/line_follower/setup.py
@@ -6,24 +6,32 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    
     data_files=[
+        # Required by ROS 2
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        
+        # package.xml
         ('share/' + package_name, ['package.xml']),
+        
+       #lanuch file
+        ('share/' + package_name + '/launch', ['launch/line_follow.launch.py']),
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='youssef',
-    maintainer_email='youssefmohamed4004306@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='Line follower ROS2 package',
+    license='MIT',
+
     entry_points={
         'console_scripts': [
+            'line_sensor_node = line_follower.line_sensor_node:main',
+            'encoder_odometry_node = line_follower.encoder_odometry_node:main',
+            'line_controller_node = line_follower.line_controller_node:main',
+            'motor_driver_node = line_follower.motor_driver_node:main',
         ],
     },
 )
